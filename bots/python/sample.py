33e98b9c5d5188aafb26ad46442a4a4d883c640c
Jack Armstrong <mackycheese21@gmail.com>
2019-06-30 08:23:46 -0700

    Debug to stderr

--- bots/python/sample.py
+++ bots/python/sample.py
@@ -2,11 +2,18 @@ import sys
 
 from python.adaptor import read_board
 
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 with open("sample.log", "w") as f:
     try:
         x = read_board(sys.stdin.buffer.read())
 
         width, height, line, turn, board = x
+
+        eprint("w =",width,", h =",height," dividing_line =",line," turn =",turn)
 
         print("4 0 1")
     except Exception as e:
