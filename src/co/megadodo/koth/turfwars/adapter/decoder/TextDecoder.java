6489151e6a01d35a5398e79c34111474765bb8bb
Max Steinberg <maxssteinberg@gmail.com>
2019-06-29 22:27:41 -0700

    askgnashdghalsdjglksadjglkadnglk/JSF;'DJg 'dAAS'

--- src/co/megadodo/koth/turfwars/adapter/decoder/TextDecoder.java
+++ src/co/megadodo/koth/turfwars/adapter/decoder/TextDecoder.java
@@ -1,29 +1,29 @@
 package co.megadodo.koth.turfwars.adapter.decoder;
 
-import co.megadodo.koth.turfwars.Action;
-import co.megadodo.koth.turfwars.ActionDestroy;
-import co.megadodo.koth.turfwars.ActionMove;
-import co.megadodo.koth.turfwars.ActionPlace;
+import co.megadodo.koth.turfwars.*;
 
 public class TextDecoder extends Decoder {
     @Override
     public Action decodeAction(String data) {
-        int val = Integer.parseInt(data);
+        String[] arr = data.split(" ");
 
         // 4 place
         // 4 destroy
         // 4 move
         // shoot
 
-        byte moveType = (byte)(val & 0x3);
+        int moveType = Integer.parseInt(arr[0]);
+        int x = Integer.parseInt(arr[1]);
+        int y = Integer.parseInt(arr[2]);
         switch(moveType) {
             case 0: // place0
-                return new ActionPlace(0, 0);
+                return new ActionPlace(x, y);
             case 1: // destroy
-                return new ActionDestroy(0, 0);
+                return new ActionDestroy(x, y);
             case 2: // move
-                return new ActionMove(0, 0);
+                return new ActionMove(x, y);
             case 3: // shoot
+                return new ActionShoot();
             default:
                 return null;
         }
